6af9c6a7ececc2f36e059bf3b27cb86ff70dd598
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-05-10 13:12:11 +0300

    generate by sequence

--- part 1/PulseGen.m
+++ part 1/PulseGen.m
@@ -1,7 +1,12 @@
-function x = PulseGen(t,dur,amp,start)
-if start == 1
-    v = amp .* ( t <=dur);
-else
-    v = amp .* ( t >=dur & t <= 2*dur);
+function x = PulseGen(t,dur,amp,sequance)
+s_dur = 0;
+e_dur = dur;
+v = zeros(1,length(t));
+for i =1:length(sequance)
+    if sequance(i) == 1
+    v = amp .* (t >=s_dur & t <= e_dur) + v;
+    end
+    s_dur = s_dur + dur;
+    e_dur = e_dur + dur;
 end
-x = v
+x = v;
