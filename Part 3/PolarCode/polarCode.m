6a1391e9737f30c3fda1aadb649565d2dafd2407
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-05-13 22:40:07 +0200

    edit some variables

--- Part 3/PolarCode/polarCode.m
+++ Part 3/PolarCode/polarCode.m
@@ -3,16 +3,18 @@ clear all;
 %%
 N_bits = 300;
 bit_seq = GenerateBits(N_bits);
-p_vec = linspace(0,0.5,500);
+p_vec = linspace(0,0.5,5);
 K = N_bits;
-N = 512;
+N = 1024;
 %% effect of P on the BER 
 packet = PolarEncode(bit_seq,K,N);
+
 BER = zeros(1,length(p_vec));
 for i =1:length(p_vec)
     rec_sample = BSC(packet,p_vec(i));
     rec_sample(rec_sample==1) = -1;    rec_sample(rec_sample==0) = 1; %PBSK
     decoded_bits = PolarDecode(rec_sample,K,N);
+    %decoded_bits = PolarDecode2(rec_sample,K,N);
     BER(i) = ComputeBER(bit_seq,decoded_bits);
 end
 figure
